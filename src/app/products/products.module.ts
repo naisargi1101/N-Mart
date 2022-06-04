e68fbedf7f146617eef5a3aaf2b4725a4ad23498
Naisargi Savaliya <naisargisavaliya1101@gmail.com>
2022-06-04 20:01:37 +0530

    shown products from json file

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,14 @@
+import { ProductDataService } from './product-data.service';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 
-
 @NgModule({
-  declarations: [
-    ProductsComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
+  declarations: [ProductsComponent],
+  imports: [CommonModule, ProductsRoutingModule, HttpClientModule],
+  providers: [ProductDataService],
 })
-export class ProductsModule { }
+export class ProductsModule {}
--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,15 +1,16 @@
+import { ProductDataService } from '../product-data.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'nm-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private pd: ProductDataService) {}
+  public products: Observable<any>;
   ngOnInit(): void {
+    this.products = this.pd.getAllProducts();
   }
-
 }
